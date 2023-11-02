0882e32aa10f0acdbb28c385df38c0fd5789ecad
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-11-02 09:33:21 -0500

    Update streamlit_app.py
    added application code from class to module.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,30 @@
-import snowflake.connector
+import streamlit 
+import snowflake.connector 
+import pandas streamlit.title('Zena\'s Amazing Athleisure Catalog') 
 
+# connect to snowflake 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor() 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") 
-my_data_row = my_cur.fetchone() 
-streamlit.text("Hello from Snowflake:") 
-streamlit.text(my_data_row)
+
+# run a snowflake query and put it all in a var called my_catalog 
+my_cur.execute("select color_or_style from catalog_for_website") 
+my_catalog = my_cur.fetchall() 
+
+# put the data into a dataframe 
+df = pandas.DataFrame(my_catalog) 
+
+# temp write the dataframe to the page so I Can see what I am working with 
+# streamlit.write(df) 
+
+# put the first column into a list 
+color_list = df[0].values.tolist() 
+
+# print(color_list) 
+
+# Let's put a pick list here so they can pick the color 
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list)) 
+
+# We'll build the image caption now, since we can 
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!' 
+
+# use the option selected to go back and get all the info from the database
