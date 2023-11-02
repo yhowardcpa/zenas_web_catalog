2ee0ac8854033cb561c223c0dd037c55f35c5d9d
yhowardcpa <148703061+yhowardcpa@users.noreply.github.com>
2023-11-02 12:39:56 -0500

    Update streamlit_app.py
    correct error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit 
 import snowflake.connector 
-import pandas streamlit.title('Zena\'s Amazing Athleisure Catalog') 
+import pandas 
+
+streamlit.title('Zena\'s Amazing Athleisure Catalog') 
 
 # connect to snowflake 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
